1f61319fd9a564f4dcac19601e53ed51d837a1f8
Judas <sinnerconsort@gmail.com>
2026-01-17 14:03:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const extensionName = 'the-tribunal';
 const PANEL_HEADER_HTML = `
 <div class="ie-right-ruler"></div>
 <div class="ie-film-bottom-text"></div>
-<span class="ie-panel-marker ie-panel-marker-first" style="top: 320px !important;">01A15</span>
+<span class="ie-panel-marker ie-panel-marker-first" style="top: 300px !important;">01A15</span>
 <span class="ie-panel-marker ie-panel-marker-bottom">01A15</span>
 <span class="ie-panel-marker-right">FELD ▼   DEVICE</span>
 
