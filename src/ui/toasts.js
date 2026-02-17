65257362406addd4d8873dd15508074babe41865
Judas <sinnerconsort@gmail.com>
2026-02-17 14:59:19 -0500

    Delete src/ui directory

--- src/ui/toasts.js
+++
@@ -1,8 +0,0 @@
-/**
- * Simple toast wrapper
- */
-export function showToast(message, type = 'info', duration = 3000) {
-    if (typeof toastr !== 'undefined') {
-        toastr[type]?.(message, 'The Tribunal', { timeOut: duration });
-    }
-} 
